97e31af0251cf558be9d46c4718a14e4876f0012
Leo <leooooo@bu.edu>
2015-10-18 17:00:08 -0400

    work on new version

---
+++ v0.5.0/control.py
@@ -0,0 +1,173 @@
+import pygame.midi
+from giantwin32 import * # keypressing stuff
+import configparser
+
+class Controller():
+
+	def __init__(self,inp,out,event_get,binds,descs,db,gui,debug=True,twitch=True):
+		self.inp = inp
+		self.out = out
+		self.event_get = event_get
+		self.binds = binds
+		self.descs = descs
+		self.db = db
+		self.gui = gui
+		self.debug = debug
+		self.reset_mode = 0
+		self.my_dict = {}
+		self.stored_n = {}
+		self.stored_diff = {}
+		self.ws = []
+		self.currgroups = [0,1]
+		self.desc_dict = {}
+		for i in range(len(binds)):
+			self.desc_dict[binds[i]] = descs[i]
+		self.lastpad = [-1, -1, -1]
+		self.twitch = twitch
+		self.setup()
+
+	def setup(self):
+		# import config as dictionary
+		Config = configparser.RawConfigParser()
+		Config.optionxform = str 
+		Config.read('vj_config.ini')
+		for o in self.binds:
+			try:
+				v = Config.get('Control',o)
+				p = Config.get('SorC',o)
+				if p == 'w': 
+					self.ws.append(o)
+					self.stored_diff[o] = 0
+				self.my_dict[v] = [o,p]
+				self.stored_n[o] = -1
+				
+			except:
+				if self.debug: print(o,' failed')
+		self.start()
+	
+
+	## KEYS
+	
+	def key_parser(self,k,o,n=1,slow=2.5):
+
+		if o == 's': #simple
+			if n > 64:
+				for _ in range((128-n)):		
+					typer(k[0])
+			else:
+				for _ in range(n):
+					typer(k[1])
+		elif o == 'c' or o == 'w': #complex or sWiper
+			oldn = self.stored_n[k]
+			if oldn == -1: 
+				self.stored_n[k] = n
+			else:
+				self.stored_n[k] = n 
+				n = n - oldn
+				while n > 4 or n < -4:
+					n = int(n // slow) + 1
+				if o == 'w': 
+					self.stored_diff[k] = self.stored_diff[k] - n			
+				if n > 0:
+					for _ in range(n):
+						typer(k[0])
+				else:
+					for _ in range(-1*n):
+						typer(k[1])
+
+		elif o == 'p': #pad
+			if n == 127:
+				if self.debug: print("set {} pad {}".format(k[0],k[1]))
+				preset = self.db[self.currgroups[int(k[0])]][int(k[1])]
+				self.lastpad = self.light(k[0],k[1],15,'g')
+				self.select_preset(preset)
+				for w in self.ws:
+					self.stored_diff[w] = 0
+
+	def select_preset(self,preset):
+		name = preset.get_name()
+		typer('l')
+		press('home')
+		print(name)
+		fast_press('down_arrow',preset.get_n())
+		press('enter','esc')
+
+	def advance(self,n):
+		if self.currgroups[0] + n >= 0 and self.currgroups[1] + n < len(self.db):
+			temp = self.lastpad
+			self.light(*temp)
+			self.currgroups = [self.currgroups[0]+n, self.currgroups[1]+n]
+			self.gui.place_presets(self.db,*self.currgroups)
+			#if temp[2] > 0:
+			if temp[0] + n == 2:
+				self.light(0,temp[1],temp[2])
+			elif temp[0] + n == -1:
+				self.light(1,temp[1],temp[2])
+
+	def reset(self,k,o,n):
+		if o == 'c':
+			if self.debug: print('resetting',k)    	
+			self.stored_n[k] = -1
+		elif o == 'w':
+			self.stored_n[k] = 0
+			d = self.stored_diff[k]
+			if self.debug: print('swiper reset',k,d)
+			self.key_parser(k,o,d)
+			self.stored_diff[k] = 0
+			self.stored_n[k] = -1
+		else:
+			self.key_parser(k,o,n)
+
+	def light(self,n,m,i=5,rga='r'): # n - set num, m - pad num, i - intensity, rga - color
+		if i < 0: i = 0
+		elif i > 15: i = 15
+		# red add 0
+		if rga == 'a': i += 64 # amber
+		elif rga == 'g': i += 112 # green
+		newpad = [int(n),int(m), i]
+		if not self.twitch: return newpad # break if not twitch 
+		self.out.write([[[151+newpad[0],60+newpad[1],newpad[2]],pygame.midi.time()]])
+		if self.lastpad != [-1, -1, -1] and self.lastpad != newpad:
+			self.out.write([[[151+self.lastpad[0],60+self.lastpad[1],0],pygame.midi.time()]])
+		return newpad
+
+	def start(self):
+		if self.debug:
+			for k,v in self.my_dict.items():
+				print(k,v)
+			#for k,v in self.desc_dict.items():
+			#	print(k,v)
+		self.gui.place_presets(self.db,*self.currgroups)
+		going = True
+		
+		while going:
+		
+		        events = self.event_get()
+		        for e in events:
+		                if e.type in [QUIT]:
+		                        going = False
+		                elif e.type==VIDEORESIZE:	
+		                	self.gui.resize(*e.dict['size'])
+		
+		        if self.inp.poll():
+		                midi_events = self.inp.read(10)
+		                the_key = str([midi_events[0][0][0],midi_events[0][0][1]])
+		                n = int(midi_events[0][0][2])
+		                try:
+		                	#if self.debug: print(the_key,n)
+		                	self.gui.write_text(self.desc_dict[self.my_dict[the_key][0]],0,0)
+		                	if self.my_dict[the_key][1] == 'r': 
+		                		if n == 127: self.reset_mode = 1 #(self.reset_mode + 1) % 4
+		                		elif n == 0: self.reset_mode = 0
+		                	if self.reset_mode != 0:  	
+		                		self.reset(self.my_dict[the_key][0],self.my_dict[the_key][1],n)
+		                		#self.reset_mode = 0
+		                	elif self.my_dict[the_key][1] == 'a' and n == 0: #advance
+		                		self.advance(1)
+		                	elif self.my_dict[the_key][1] == 'd' and n == 0: #decrease
+		                		self.advance(-1)
+		                	else: 
+		                		self.key_parser(self.my_dict[the_key][0],self.my_dict[the_key][1],n)
+		                except:
+		                	if self.debug: print(the_key,n)
+		
---
+++ v0.5.0/mdvj.py
@@ -0,0 +1,127 @@
+import tkinter as tk
+from PIL import ImageTk,Image
+
+from db import Database
+from control_md import ControlMD
+
+class MainGui:
+	""" the main gui """
+
+	def __init__(self,root):
+		# mdvj stuff
+		self.db = Database("C:/Program Files (x86)/Winamp/plugins/milkdrop2/presets") # will update later to select your folder first
+		self.db.start()
+		starting_len = len(self.db)
+
+		self.mdc = ControlMD()
+		# tk stuff
+		self.root = root
+		self.frame = tk.Frame(root)
+		self.padframe = tk.Frame(self.frame)
+
+		self.padgroup_l_n = -1
+		self.padgroup_r_n = -1
+		if starting_len > 1:
+			self.padgroup_l_n = 0
+			self.padgroup_r_n = 1
+			self.padgroup_l = PadGroup(self,self.db[0])
+			self.padgroup_r = PadGroup(self,self.db[1])
+		else:
+			if starting_len > 0:
+				self.padgroup_l_n = 0
+				self.padgroup_l = PadGroup(self,self.db[0])
+			else:
+				self.padgroup_l = PadGroup(self)
+			self.padgroup_r = PadGroup(self)
+
+		self.controlframe = tk.Frame(self.frame)
+		self.prev_col_button = tk.Button(self.controlframe,text="<",command=self.go_l,pady=0)
+		self.next_col_button = tk.Button(self.controlframe,text=">",command=self.go_r,pady=0)
+		self.check_lr()
+		self.next_col_button.pack(side=tk.RIGHT,anchor=tk.SE)
+		self.prev_col_button.pack(side=tk.RIGHT,anchor=tk.SE)
+		self.padframe.pack()
+		self.controlframe.pack()
+		self.frame.pack()
+		
+	def check_lr(self):
+		if self.padgroup_l_n > 0:
+			# enable l
+			self.prev_col_button.config(state='normal')
+		else:
+			# bad
+			self.prev_col_button.config(state='disabled')
+		if self.padgroup_r_n < len(self.db) - 1:
+			# enable r
+			self.next_col_button.config(state='normal')
+		else:
+			# bad
+			self.next_col_button.config(state='disabled')
+
+	def go_l(self):
+		self.padgroup_l_n -= 1
+		self.padgroup_r_n -= 1
+		self.padgroup_l.init_containers(self.db[self.padgroup_l_n])
+		self.padgroup_r.init_containers(self.db[self.padgroup_r_n])
+		self.check_lr()
+
+	def go_r(self):
+		self.padgroup_l_n += 1
+		self.padgroup_r_n += 1
+		self.padgroup_l.init_containers(self.db[self.padgroup_l_n])
+		self.padgroup_r.init_containers(self.db[self.padgroup_r_n])
+		self.check_lr()
+
+
+class PadGroup:
+	""" stores 8 presets """
+
+	def __init__(self,parent,presets=None):
+		self.parent = parent
+		self.preset_containers = [None]*8
+
+		self.frame = tk.Frame(parent.padframe,borderwidth=5,relief=tk.RIDGE)
+		self.frame.pack(side=tk.LEFT)
+		if presets:
+			self.init_containers(presets)
+
+	def init_containers(self,presets):
+		max_len = len(presets)
+		for r in range(2):
+			for c in range(4):
+				i = r * 4 + c
+				if i >= max_len :
+					return
+				#print(self.db[r][c])
+				self.preset_containers[i] = PresetContainer(self,presets[i])
+				self.preset_containers[i].grid(row=r,column=c)
+
+
+class PresetContainer:
+	""" stores a single preset """
+
+	def __init__(self,padgroup,preset):
+		self.padgroup = padgroup
+		self.preset = preset
+		self.img = Image.open(self.preset.img)
+		self.img = self.img.resize((150, 150), Image.ANTIALIAS)
+		self.img = ImageTk.PhotoImage(self.img)
+		self.label = tk.Label(self.padgroup.frame,image=self.img,text="",compound='top',width=150) 
+		self.change_text(self.preset.name)
+		self.grid = self.label.grid
+		self.label.bind('<ButtonPress-1>',self.select)
+	def change_text(self,new_text):
+		new_text = str(new_text)
+		if len(new_text) > 18:
+			new_text = new_text[:17] + ".."
+		self.label.config(text=new_text)
+
+	def select(self,event):
+		self.padgroup.parent.mdc.select_preset(self.preset)
+		
+
+if __name__ == '__main__':
+	root = tk.Tk()
+	root.title("mdvj")
+	gui = MainGui(root)
+	root.mainloop()
